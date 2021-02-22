c685666aa0b33c2a005f95644f184c78ba1531f0
Aline <Alinewicz@gmail.com>
2021-02-22 17:16:58 -0300

    Atualizando.

--- TypeScript - Parte I/alurabank/app/ts/app.ts
+++ TypeScript - Parte I/alurabank/app/ts/app.ts
@@ -1,4 +1,7 @@
-let negociacao = new Negociacao(new Date(), 1, 100);
-console.log(negociacao);
+const controller = new NegociacaoController();
+document
+    .querySelector('.form')
+    .addEventListener('submit', controller.adiciona.bind(controller));
+    
 
 
